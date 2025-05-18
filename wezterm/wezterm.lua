a0435b20faacd6c4ae248f910a3e2cc8af1d8b7e
DmytroKubatko <havoc3144@gmail.com>
2025-05-18 20:49:59 +0300

    auto align window to the right on the startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,4 +8,25 @@ local keyTables = require("key-tables")
 
 local config = lib.mergeTables(windowConfig, fontConfig, formatTitle, keysConfig, keyTables)
 
+wezterm.on("gui-startup", function(cmd)
+	-- Get the list of screens
+	local screen = wezterm.gui.screens().active
+
+	-- Get the dimensions of the active screen
+	local screen_width = screen.width
+	local screen_height = screen.height
+
+	local window_width = 180
+
+	-- Create a new window with the specified position and size
+	local tab, pane, window = wezterm.mux.spawn_window({
+		position = {
+			x = screen_width / 2 + window_width,
+			y = screen_height / 2,
+		},
+		width = window_width,
+		height = 90,
+	})
+end)
+
 return config
